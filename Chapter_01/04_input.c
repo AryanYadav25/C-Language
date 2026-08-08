c6e90dd65a36dd2941d4727ea441c1ca735ffdff
AryanYadav <aryan.yofficial@gmail.com>
2026-08-08 23:38:09 +0530

    Escape sequence

---
+++ Chapter_01/04_input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("The Value Of a is: %d", a);
+    return 0;
+}
---
+++ Chapter_01/05_escape_sequence.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char ch = 'a';
+    ch = '\n'; // Represents new line 
+    printf("Hey my name is \n Aryan\n"); // put aryan in next line
+    printf("Hey my name is \t Aryan\n"); // put a gap between is and aryan
+    printf("Hey my name is \" Aryan\n"); // add a double quote after is 
+    printf("Hey my name is \\ Aryan\n"); // add a \ after is 
+
+    return 0;
+}
