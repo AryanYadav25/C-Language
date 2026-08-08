2bf9d890b64b7c196be79c6dd4990307ce6c53de
AryanYadav <aryan.yofficial@gmail.com>
2026-08-08 22:55:47 +0530

    Learned data types

---
+++ Chapter_01/02_data_types.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a;
+    a = 10;
+    //OR
+    int A = 12;
+
+    float b;
+    b = 3.4;
+    //OR
+    float B = 4.5;
+
+    char c;
+    c = 'H';
+    //OR
+    char d = 'n';
+    return 0;
+}
