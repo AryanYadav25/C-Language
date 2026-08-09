262049a91fcfba691f61dbe1cb9ad881f361a53b
AryanYadav <aryan.yofficial@gmail.com>
2026-08-10 01:02:01 +0530

    Questions

---
+++ Chapter_02_practice/02_question.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // question is what data type will be the result 
+    // 3.0/8 - 2
+
+    printf("%f\n", 3.0/8 -2);
+    printf("%d", 3.0/8 -2);
+    return 0;
+}
