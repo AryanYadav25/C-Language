e18a51cf299581655659d1d9c855de786f6b1afd
AryanYadav <aryan.yofficial@gmail.com>
2026-08-10 01:00:51 +0530

    First question

---
+++ Chapter_02_practice/01_question.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // which of the following is invalid 
+    //a. int a = 1, int b = a        // b = 1
+    //b. int v = 3*3         // 9 
+    //c. char dt = '21 dec 2020'                // this one is wrong // char can only hold one bit either 0 or 1 
+
+    printf("Hello what's up doing good?\nif not then make it good :)");
+    return 0;
+}
