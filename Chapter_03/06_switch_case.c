5dacfa6a8e407bfbe3ac53541d10f5cfe3b9047d
AryanYadav <aryan.yofficial@gmail.com>
2026-08-12 21:10:47 +0530

    Switch case

---
+++ Chapter_03/06_switch_case.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter a value:");
+    scanf("%d", &a);
+
+switch(a){
+    case 45:
+    printf("You have entered 45\n");
+    break;
+    case 69:
+    printf("Have some shame writing code\n ");
+    break;
+    case 12:
+    printf("Now you want this 12\n");
+    break;
+}
+
+    return 0;
+}
